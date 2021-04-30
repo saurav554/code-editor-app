801018cc0cb7ae6f1039c8c4558eb8291f2595ec
saurav <avisaurav485@gmail.com>
2021-04-30 11:42:42 +0530

    Updates

---
+++ precache-manifest.998ca901d8ecdf61b919627ce719d304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe4fe0aa4f0a6671da90",
+    "url": "/code-editor-app/static/css/main.0e53ab3c.chunk.css"
+  },
+  {
+    "revision": "fe4fe0aa4f0a6671da90",
+    "url": "/code-editor-app/static/js/main.13e77241.chunk.js"
+  },
+  {
+    "revision": "6462bb72c90454e14916",
+    "url": "/code-editor-app/static/js/runtime~main.e49d6623.js"
+  },
+  {
+    "revision": "5df510a9bf8c4d2cec2d",
+    "url": "/code-editor-app/static/css/2.f68f3480.chunk.css"
+  },
+  {
+    "revision": "5df510a9bf8c4d2cec2d",
+    "url": "/code-editor-app/static/js/2.f97bc54f.chunk.js"
+  },
+  {
+    "revision": "1ed1a6236fb80f8bdef8059b9d54acc3",
+    "url": "/code-editor-app/index.html"
+  }
+];
